0ae29653b89699faba91a6d1d8e815d68c040e5a
Daniel Ugarov <tyradire@yandex.ru>
2024-06-06 01:22:38 +0300

    fix: wishRouter path

--- routes/wishRouter.js
+++ routes/wishRouter.js
@@ -4,6 +4,6 @@ const wishController = require('../controllers/wishController');
 
 router.patch('/add/:id', wishController.addToWish);
 router.patch('/delete/:id', wishController.deleteFromWish);
-router.get('/getwish', wishController.getWishList);
+router.get('/getwishlist', wishController.getWishList);
 
 module.exports = router;
